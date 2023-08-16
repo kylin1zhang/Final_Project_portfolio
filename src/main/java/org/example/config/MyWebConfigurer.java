a4877c408c457fa55e9c77f84e3df0124a488498
tairi <tairi.goto@mail.utoronto.ca>
2023-08-16 20:59:58 +0900

    Resolved CORS error on frontend

--- src/main/java/org/example/config/MyWebConfigurer.java
+++ src/main/java/org/example/config/MyWebConfigurer.java
@@ -15,7 +15,7 @@ public class MyWebConfigurer implements WebMvcConfigurer {
          */
         corsRegistry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .allowedHeaders("*")
                 .maxAge(3600);
