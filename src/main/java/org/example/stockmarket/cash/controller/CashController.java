4f980ed1f1670eb73339f1a01d8e2459e1489bb4
chaoran <2030785539@qq.com>
2023-08-17 11:47:33 +0800

    add function

--- src/main/java/org/example/stockmarket/cash/controller/CashController.java
+++ src/main/java/org/example/stockmarket/cash/controller/CashController.java
@@ -1,9 +1,12 @@
 package org.example.stockmarket.cash.controller;
 
+import org.example.stockmarket.cash.entity.Cash;
 import org.example.stockmarket.cash.service.CashService;
+import org.example.stockmarket.stocks.stock.entity.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/cash")
@@ -12,5 +15,14 @@ public class CashController {
     @Autowired
     CashService cashService;
 
+    @GetMapping
+    public List<Cash> getAllCash() {
+        return cashService.getAllCash();
+    }
 
+    @PostMapping
+    public List<Cash> getAllCashWithChange(@RequestParam("id") int id,
+                                           @RequestParam("change") int change){
+        return cashService.getAllCashWithChange(id,change);
+    }
 }
