5efcee7195fafa1a5ac9beb5e199f3e0d63363e9
tairi <tairi.goto@mail.utoronto.ca>
2023-08-17 18:32:16 +0900

    Fixed cors issue with cash and stock endpoints

--- src/main/java/org/example/stockmarket/cash/controller/CashController.java
+++ src/main/java/org/example/stockmarket/cash/controller/CashController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:8080", "http://localhost:3000"})
 @RequestMapping(value = "/api/v1/cash")
 public class CashController {
 
--- src/main/java/org/example/stockmarket/stocks/stock/controller/StockController.java
+++ src/main/java/org/example/stockmarket/stocks/stock/controller/StockController.java
@@ -18,7 +18,7 @@ stock class, which ignores news, earnings, and price history fields.
  */
 @RestController
 @RequestMapping(value = "/api/v1/stocks")
-@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://127.0.0.1:5501"})
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:8080", "http://localhost:3000"})
 @AllArgsConstructor
 @SuppressWarnings("unused")
 public class StockController {
