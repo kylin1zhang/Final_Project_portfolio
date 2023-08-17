e4b100224e1968d0525fd7e19148f631dbc1ddc0
chaoran <2030785539@qq.com>
2023-08-17 15:39:06 +0800

    add function

--- src/main/java/org/example/stockmarket/cash/repository/CashRepository.java
+++ src/main/java/org/example/stockmarket/cash/repository/CashRepository.java
@@ -4,6 +4,9 @@ import org.example.stockmarket.cash.entity.Cash;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CashRepository extends JpaRepository<Cash,Integer> {
+    public List<Cash> findByName(String name);
 }
--- src/main/java/org/example/stockmarket/cash/service/CashService.java
+++ src/main/java/org/example/stockmarket/cash/service/CashService.java
@@ -6,6 +6,7 @@ import org.example.stockmarket.stocks.stock.entity.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,5 +24,18 @@ public class CashService {
         cash.setBalance(cashRepository.findById(id).get().getBalance()+change);
         cashRepository.save(cash);
         return cashRepository.findAll();
+    }
+
+    public void createCashAccount(String name,int change){
+        if(!cashRepository.findByName(name).isEmpty()){
+            throw new NullPointerException("Cannot Create Account With existing Username");
+        }
+        Cash cash = new Cash();
+        cash.setId((int) (cashRepository.count()+1));
+        cash.setCreated(new Date());
+        cash.setModified(new Date());
+        cash.setName(name);
+        cash.setBalance(change);
+        cashRepository.save(cash);
     }
 }
