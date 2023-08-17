8eb8eb340d3f036cf90e556e714b4e302552fa1a
chaoran <2030785539@qq.com>
2023-08-17 16:24:20 +0800

    delete    @GeneratedValue

--- src/main/java/org/example/stockmarket/cash/entity/Cash.java
+++ src/main/java/org/example/stockmarket/cash/entity/Cash.java
@@ -15,7 +15,6 @@ import java.util.Date;
 public class Cash {
 
     @Id
-    @GeneratedValue
     private int id;
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
     @DateTimeFormat
