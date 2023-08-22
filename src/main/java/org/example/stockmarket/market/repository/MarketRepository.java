b339e17f9edbc8f157619aa43ce018315da93c24
zhangzhaofei <zhangzhaofei123@foxmail.com>
2023-08-22 16:12:58 +0800

    delete market part

--- src/main/java/org/example/stockmarket/market/repository/MarketRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.stockmarket.market.repository;
-
-import org.example.stockmarket.market.entity.Market;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarketRepository extends JpaRepository<Market, Integer> {
-}
